e5da801e93bc88434ced84d9e5e699ed3af6834e
Olalekan Yusuf <olasyusuf@gmail.com>
2025-10-19 10:07:27 +0100

    feat: implement Cancellation Policy & and Ground Rules Component - components/booking/CancellationPolicy.tsx

--- components/booking/OrderSummary.tsx
+++ components/booking/OrderSummary.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const OrderSummary: React.FC<{ bookingDetails: any }> = ({ bookingDetails }) => (
   <div className="bg-white p-6 shadow-md rounded-lg">
     <h2 className="text-xl font-semibold">Review Order Details</h2>
